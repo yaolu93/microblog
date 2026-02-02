6fe0631ffa94294931d88820838fc0d9e0e7adb4
Achilles <lytreetree@gmail.com>
2026-02-02 12:46:49 +0000

    added

--- practice_python/validPalindrome.py
+++ practice_python/validPalindrome.py
@@ -1,23 +1,27 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+
         left = 0
         right = len(s) - 1
 
         while left < right:
             if s[left] == s[right]:
-                print(f"left = {left}")
-                print(f"right = {right}")
                 left += 1
                 right -= 1
+
             else:
                 return (self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1))
 
         return True
+                
 
+    
     def isPalindrome(self, s: str, left: int, right: int) -> bool:
         while left < right:
+
             if s[left] != s[right]:
                 return False
+
             left += 1
             right -= 1
         return True
