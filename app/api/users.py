0aad538f0988664db91454240933f72133ba56ff
Achilles <lytreetree@gmail.com>
2026-03-10 21:36:42 +0000

    remove unused

--- app/api/users.py
+++ app/api/users.py
@@ -1,10 +1,9 @@
 import sqlalchemy as sa
-from flask import request, url_for, abort
+from flask import request
 from app import db
 from app.models import User
 from app.api import bp
 from app.api.auth import token_auth
-from app.api.errors import bad_request
 
 
 @bp.route('/users/<int:id>', methods=['GET'])
@@ -42,40 +41,7 @@ def get_following(id):
                                    'api.get_following', id=id)
 
 
-@bp.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    if 'username' not in data or 'email' not in data or 'password' not in data:
-        return bad_request('must include username, email and password fields')
-    if db.session.scalar(sa.select(User).where(
-            User.username == data['username'])):
-        return bad_request('please use a different username')
-    if db.session.scalar(sa.select(User).where(
-            User.email == data['email'])):
-        return bad_request('please use a different email address')
-    user = User()
-    user.from_dict(data, new_user=True)
-    db.session.add(user)
-    db.session.commit()
-    return user.to_dict(), 201, {'Location': url_for('api.get_user',
-                                                     id=user.id)}
 
 
-@bp.route('/users/<int:id>', methods=['PUT'])
-@token_auth.login_required
-def update_user(id):
-    if token_auth.current_user().id != id:
-        abort(403)
-    user = db.get_or_404(User, id)
-    data = request.get_json()
-    if 'username' in data and data['username'] != user.username and \
-        db.session.scalar(sa.select(User).where(
-            User.username == data['username'])):
-        return bad_request('please use a different username')
-    if 'email' in data and data['email'] != user.email and \
-        db.session.scalar(sa.select(User).where(
-            User.email == data['email'])):
-        return bad_request('please use a different email address')
-    user.from_dict(data, new_user=False)
-    db.session.commit()
-    return user.to_dict()
+
+
